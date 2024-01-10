b91cdcb5f9e39a2052b4b29cc19b1ce2a237acba
Roquv13 <czarekolszewski99@gmail.com>
2024-01-10 21:06:23 +0100

    Implement Listener, add getLogger.info to onEnable and onDisable

--- src/main/java/org/plugins/pluginmc/Main.java
+++ src/main/java/org/plugins/pluginmc/Main.java
@@ -1,17 +1,18 @@
 package org.plugins.pluginmc;
 
+import org.bukkit.ChatColor;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class Main extends JavaPlugin {
+public final class Main extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getLogger().info(ChatColor.GREEN + "Plugin > started");
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        getLogger().info(ChatColor.GREEN + "Plugin > stopped");
     }
 }
