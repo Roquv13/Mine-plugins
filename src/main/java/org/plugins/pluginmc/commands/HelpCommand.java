b574670772b78fcd14ea8b4d919dc0e76a58c12c
Roquv13 <czarekolszewski99@gmail.com>
2024-01-11 02:03:20 +0100

    Add new messages for help command

--- src/main/java/org/plugins/pluginmc/commands/HelpCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/HelpCommand.java
@@ -4,12 +4,16 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.plugins.pluginmc.utils.ChatUtil;
 
 public class HelpCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] strings) {
         if (sender instanceof Player) {
             Player player = ((Player) sender).getPlayer();
+            player.sendMessage(ChatUtil.colorize("&7/rank - informations about ranks"));
+            player.sendMessage(ChatUtil.colorize("&8/guild - informations about guilds"));
+            player.sendMessage(ChatUtil.colorize("&b/tnt - informations about tnt"));
         } else {
             System.out.println("Only for players!");
         }
