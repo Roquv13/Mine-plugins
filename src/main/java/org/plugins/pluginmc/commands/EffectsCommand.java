12f0993c9e87f992557a6d1757428568fa763a63
Roquv13 <czarekolszewski99@gmail.com>
2024-01-11 23:41:55 +0100

    Add onCommand gui for players

--- src/main/java/org/plugins/pluginmc/commands/EffectsCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/EffectsCommand.java
@@ -3,6 +3,7 @@ package org.plugins.pluginmc.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.plugins.pluginmc.gui.Gui;
 
 public class EffectsCommand implements CommandExecutor {
@@ -15,6 +16,13 @@ public class EffectsCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        return false;
+        if (sender instanceof Player) {
+            Player player = (Player) sender;
+            gui.openGui(player);
+        } else {
+            sender.sendMessage("Only for players");
+        }
+
+        return true;
     }
 }
