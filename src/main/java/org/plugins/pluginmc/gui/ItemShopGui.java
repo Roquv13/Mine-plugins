e6fa3a6128d53fa03305f3542af85b468e69cb4d
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-13 16:53:46 +0100

    Refactor ItemShopGui

--- src/main/java/org/plugins/pluginmc/gui/ItemShopGui.java
+++ src/main/java/org/plugins/pluginmc/gui/ItemShopGui.java
@@ -3,6 +3,7 @@ package org.plugins.pluginmc.gui;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -10,20 +11,26 @@ import org.plugins.pluginmc.utils.ChatUtil;
 
 import java.util.Collections;
 
-public class ItemShopGui {
+public class ItemShopGui implements Listener {
 
     private final String guiName = ChatUtil.colorize("&6&lItem &2&lShop");
+
+    private ItemStack createGoldIngotItem() {
+        return createItem(Material.GOLD_INGOT, "&e&lGold Ingot", "&cPrice: &l&a5 &l&6coins ");
+    }
+    private ItemStack createDiamondItem() {
+        return createItem(Material.DIAMOND, "&b&lDiamond", "&cPrice: &l&a20 &l&6coins ");
+    }
+    private ItemStack createNetheriteBrickItem() {
+        return createItem(Material.NETHER_BRICK, "&8&lNetherite", "&cPrice: &l&a40 &l&6coins ");
+    }
 
     public void openGui(Player player) {
         Inventory gui = Bukkit.createInventory(player, 9, guiName);
 
-        ItemStack gold = createItem(Material.GOLD_INGOT, "&e&lGold Ingot", "&cPrice: &l&a5 &l&6coins ");
-        ItemStack diamond = createItem(Material.DIAMOND, "&b&lDiamond", "&cPrice: &l&a20 &l&6coins ");
-        ItemStack netherite = createItem(Material.NETHER_BRICK, "&8&lNetherite", "&cPrice: &l&a40 &l&6coins ");
-
-        gui.setItem(0, gold);
-        gui.setItem(1, diamond);
-        gui.setItem(2, netherite);
+        gui.setItem(0, createGoldIngotItem());
+        gui.setItem(1, createDiamondItem());
+        gui.setItem(2, createNetheriteBrickItem());
 
         player.openInventory(gui);
     }
