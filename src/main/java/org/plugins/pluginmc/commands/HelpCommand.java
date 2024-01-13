e2a41647b75ef8f9fd09607e506acd0b0998202e
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-13 03:57:21 +0100

    Add help for command effects

--- src/main/java/org/plugins/pluginmc/commands/HelpCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/HelpCommand.java
@@ -11,8 +11,14 @@ public class HelpCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (args.length == 0) {
             showDefaultHelp(sender);
-        } else if (args.length == 1 && args[0].equalsIgnoreCase("itemshop")) {
-            showItemShopHelp(sender);
+        } else if (args.length == 1) {
+            if (args[0].equalsIgnoreCase("itemshop")) {
+                showItemShopHelp(sender);
+            }
+
+            if (args[0].equalsIgnoreCase("effects")) {
+                showEffectsHelp(sender);
+            }
         }
         return false;
     }
@@ -35,6 +41,13 @@ public class HelpCommand implements CommandExecutor {
             player.sendMessage("/itemshop <player> <item> - Buy an item from item shop");
         } else {
             System.out.println("Only for players");
+        }
+    }
+
+    private void showEffectsHelp(CommandSender sender) {
+        if (sender instanceof Player) {
+            Player player = ((Player) sender).getPlayer();
+            player.sendMessage("/effects <player> - Open Gui with effects that can be applied to player");
         }
     }
 }
