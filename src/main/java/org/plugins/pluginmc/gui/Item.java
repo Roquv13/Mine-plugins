e9fbcfa70f5f6b32add06278a16ae454239c4b7f
Roquv13 <czarekolszewski99@gmail.com>
2024-01-14 23:12:14 +0100

    Add new class Item with method create

---
+++ src/main/java/org/plugins/pluginmc/gui/Item.java
@@ -0,0 +1,21 @@
+package org.plugins.pluginmc.gui;
+
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.plugins.pluginmc.utils.ChatUtil;
+
+import java.util.Collections;
+
+public class Item {
+
+    public ItemStack create(Material material, String name, String lore) {
+        ItemStack itemStack = new ItemStack(material);
+        ItemMeta itemMeta = itemStack.getItemMeta();
+        itemMeta.setDisplayName(ChatUtil.colorize(name));
+        itemMeta.setLore(Collections.singletonList(ChatUtil.colorize(lore)));
+        itemStack.setItemMeta(itemMeta);
+        return itemStack;
+    }
+
+}
