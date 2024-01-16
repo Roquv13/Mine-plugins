60e0fa04fd78f18ae4a53d22b3330142b349f623
Roquv13 <czarekolszewski99@gmail.com>
2024-01-16 20:00:46 +0100

    Add statements, only OP players can use this command

--- src/main/java/org/plugins/pluginmc/commands/ChatCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/ChatCommand.java
@@ -7,7 +7,20 @@ import org.bukkit.command.CommandSender;
 public class ChatCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        
+        if (!sender.isOp()) return true;
+
+        if (args.length != 1) return true;
+
+        switch (args[0]) {
+            case "on":
+                break;
+            case "off":
+                break;
+            case "clear":
+                break;
+            default:
+                sender.sendMessage("/chat [on/off/clear]");
+        }
 
         return false;
     }
