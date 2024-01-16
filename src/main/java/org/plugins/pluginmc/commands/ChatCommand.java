aded732abe01a424b93f5346030f371dcf19f55c
Roquv13 <czarekolszewski99@gmail.com>
2024-01-16 19:57:43 +0100

    Add Chat command

---
+++ src/main/java/org/plugins/pluginmc/commands/ChatCommand.java
@@ -0,0 +1,14 @@
+package org.plugins.pluginmc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class ChatCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        
+
+        return false;
+    }
+}
