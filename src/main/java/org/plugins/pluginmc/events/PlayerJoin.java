6609c598c4cd803008914d60a3baa5c6c82411f0
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 00:53:27 +0100

    Event handler with getting player

--- src/main/java/org/plugins/pluginmc/events/PlayerJoin.java
+++ src/main/java/org/plugins/pluginmc/events/PlayerJoin.java
@@ -1,6 +1,14 @@
 package org.plugins.pluginmc.events;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoin implements Listener {
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+    }
 }
