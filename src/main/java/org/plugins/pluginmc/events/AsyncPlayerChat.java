451a8ff56614c8ca1e988ff499dac3178d39818f
Roquv13 <czarekolszewski99@gmail.com>
2024-01-17 21:45:33 +0100

    Add Event Handler

--- src/main/java/org/plugins/pluginmc/events/AsyncPlayerChat.java
+++ src/main/java/org/plugins/pluginmc/events/AsyncPlayerChat.java
@@ -1,6 +1,13 @@
 package org.plugins.pluginmc.events;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class AsyncPlayerChat implements Listener {
+    @EventHandler
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
+        Player player = event.getPlayer();
+    }
 }
