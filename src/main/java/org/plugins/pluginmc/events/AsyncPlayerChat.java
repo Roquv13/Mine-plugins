5fa0fdba7c69e7b1c93cbeb1f82a410ef57ec6c1
Roquv13 <czarekolszewski99@gmail.com>
2024-01-17 21:54:33 +0100

    Sending message if chat is off

--- src/main/java/org/plugins/pluginmc/events/AsyncPlayerChat.java
+++ src/main/java/org/plugins/pluginmc/events/AsyncPlayerChat.java
@@ -4,10 +4,20 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
+import org.plugins.pluginmc.Main;
 
 public class AsyncPlayerChat implements Listener {
     @EventHandler
     public void onAsyncPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
+
+        // Op can use chat
+        if (player.isOp()) return;
+
+        // If chat is on, stop code
+        if (Main.isChatEnabled) return;
+
+        event.setCancelled(true);
+        player.sendMessage("Chat is off.");
     }
 }
