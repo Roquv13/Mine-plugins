644e520dc89743f0835351133b4c9d31727c46ff
Roquv13 <czarekolszewski99@gmail.com>
2024-01-19 00:00:06 +0100

    Add loop for clicked item, check if clicked inventory is player inventory

--- src/main/java/org/plugins/pluginmc/events/InventoryClick.java
+++ src/main/java/org/plugins/pluginmc/events/InventoryClick.java
@@ -5,10 +5,16 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryAction;
 import org.bukkit.event.inventory.InventoryClickEvent;
 
+import org.bukkit.event.inventory.InventoryType;
+import org.plugins.pluginmc.Main;
 import org.plugins.pluginmc.api.DropApi;
 import org.plugins.pluginmc.gui.DropGui;
+import org.plugins.pluginmc.objects.DropChance;
+
+import static org.plugins.pluginmc.events.BlockBreak.drops;
 
 public class InventoryClick implements Listener {
 
@@ -16,26 +22,42 @@ public class InventoryClick implements Listener {
 
     @EventHandler
     public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getClickedInventory() == null) return;
+        if (event.getClickedInventory() == null || event.getCurrentItem() == null || event.getCurrentItem().getType() == Material.AIR) {
+            return;
+        }
 
         Player player = (Player) event.getWhoClicked();
 
-        if (!ChatColor.stripColor(event.getView().getTitle()).equalsIgnoreCase("Drop GUI")) return;
+        // Check if the clicked inventory is the player's inventory
+        if (event.getClickedInventory().getType() == InventoryType.PLAYER) {
+            return;
+        }
+
+        // Check if the clicked inventory title is "Drop GUI"
+        if (!ChatColor.stripColor(event.getView().getTitle()).equalsIgnoreCase("Drop Menu")) {
+            return;
+        }
 
         event.setCancelled(true);
 
-        if (event.getCurrentItem() == null) return;
-
         Material clickedType = event.getCurrentItem().getType();
 
-        if (dropApi.getDisabledDrops(player).contains(clickedType)) {
-            dropApi.enableDrop(player, clickedType);
-        } else {
-            dropApi.disableDrop(player, clickedType);
+        // Check if the clicked item is part of your GUI items
+        for (DropChance dropChance : drops) {
+            if (dropChance.getMaterial() == clickedType) {
+                if (dropApi.getDisabledDrops(player).contains(clickedType)) {
+                    dropApi.enableDrop(player, clickedType);
+                } else {
+                    dropApi.disableDrop(player, clickedType);
+                }
+
+                player.closeInventory();
+                player.openInventory(DropGui.getInventory(player));
+                break;
+            }
         }
-
-        player.closeInventory();
-        player.openInventory(DropGui.getInventory(player));
     }
-
 }
+
+
+
