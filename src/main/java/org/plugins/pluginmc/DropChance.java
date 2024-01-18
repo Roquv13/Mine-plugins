4267d46e44ac21a3a0536362e87f97c4ab804e84
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 06:03:20 +0100

    Add DropChance with setters and getters

---
+++ src/main/java/org/plugins/pluginmc/DropChance.java
@@ -0,0 +1,30 @@
+package org.plugins.pluginmc;
+
+import org.bukkit.Material;
+
+public class DropChance {
+
+    private Material material;
+    private int chance;
+
+    public DropChance(Material material, int chance) {
+        this.material = material;
+        this.chance = chance;
+    }
+
+    public Material getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(Material material) {
+        this.material = material;
+    }
+
+    public int getChance() {
+        return chance;
+    }
+
+    public void setChance(int chance) {
+        this.chance = chance;
+    }
+}
