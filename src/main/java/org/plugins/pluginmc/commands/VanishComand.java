46ade93ac09a88d3dfea6f1dbfda08753bec60fb
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 01:06:18 +0100

    Add player set to visible

--- src/main/java/org/plugins/pluginmc/commands/VanishComand.java
+++ src/main/java/org/plugins/pluginmc/commands/VanishComand.java
@@ -1,10 +1,12 @@
 package org.plugins.pluginmc.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
+import org.plugins.pluginmc.Main;
 
 public class VanishComand implements CommandExecutor {
     @Override
@@ -14,5 +16,14 @@ public class VanishComand implements CommandExecutor {
         Player player = (Player) sender;
 
         if (!player.hasPermission("vanish.admin")) return true;
+
+        if (Main.invisiblePlayers.contains(player)) {
+            for (Player players : Bukkit.getOnlinePlayers()) {
+                players.showPlayer(player);
+            }
+            Main.invisiblePlayers.remove(player);
+
+            player.sendMessage("You are visible!");
+        }
     }
 }
