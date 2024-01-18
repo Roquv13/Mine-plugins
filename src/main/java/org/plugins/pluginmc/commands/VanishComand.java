611fc3df919fabd728f2ed98902dfcb9c3a475a4
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 01:02:11 +0100

    Add Vanish command class

---
+++ src/main/java/org/plugins/pluginmc/commands/VanishComand.java
@@ -0,0 +1,12 @@
+package org.plugins.pluginmc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class VanishComand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        return false;
+    }
+}
