7be26bc14a3c3fb0eaed6a01d310db7ee4b2b5b2
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 01:04:05 +0100

    Return true if player has no permission or sender is console

--- src/main/java/org/plugins/pluginmc/commands/VanishComand.java
+++ src/main/java/org/plugins/pluginmc/commands/VanishComand.java
@@ -3,10 +3,16 @@ package org.plugins.pluginmc.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 public class VanishComand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        return false;
+        if (sender instanceof ConsoleCommandSender) return true;
+
+        Player player = (Player) sender;
+
+        if (!player.hasPermission("vanish.admin")) return true;
     }
 }
