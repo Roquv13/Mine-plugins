a3f9ea1fc2d88c444303736a8bca349f14e14446
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 21:44:18 +0100

    Add InventoryClick event

--- src/main/java/org/plugins/pluginmc/events/InventoryClick.java
+++ src/main/java/org/plugins/pluginmc/events/InventoryClick.java
@@ -1,4 +1,41 @@
 package org.plugins.pluginmc.events;
 
-public class InventoryClick {
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+
+import org.plugins.pluginmc.api.DropApi;
+import org.plugins.pluginmc.gui.DropGui;
+
+public class InventoryClick implements Listener {
+
+    DropApi dropApi = new DropApi();
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getClickedInventory() == null) return;
+
+        Player player = (Player) event.getWhoClicked();
+
+        if (!ChatColor.stripColor(event.getView().getTitle()).equalsIgnoreCase("Drop GUI")) return;
+
+        event.setCancelled(true);
+
+        if (event.getCurrentItem() == null) return;
+
+        Material clickedType = event.getCurrentItem().getType();
+
+        if (dropApi.getDisabledDrops(player).contains(clickedType)) {
+            dropApi.enableDrop(player, clickedType);
+        } else {
+            dropApi.disableDrop(player, clickedType);
+        }
+
+        player.closeInventory();
+        player.openInventory(DropGui.getInventory(player));
+    }
+
 }
