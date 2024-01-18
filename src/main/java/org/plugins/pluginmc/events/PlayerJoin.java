853783c2fe6cb011b5591d86c12027983f737574
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 01:01:01 +0100

    Hide invisible players for users thats log in

--- src/main/java/org/plugins/pluginmc/events/PlayerJoin.java
+++ src/main/java/org/plugins/pluginmc/events/PlayerJoin.java
@@ -4,11 +4,16 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.plugins.pluginmc.Main;
 
 public class PlayerJoin implements Listener {
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer();
+
+        for (Player invisiblePlayer : Main.invisiblePlayers) {
+            player.hidePlayer(invisiblePlayer);
+        }
     }
 }
