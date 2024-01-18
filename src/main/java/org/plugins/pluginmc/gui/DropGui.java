153f9d93ecc5fd1210cfc8df3537696933f1d6ca
Roquv13 <czarekolszewski99@gmail.com>
2024-01-18 23:01:34 +0100

    Delete unused imports

--- src/main/java/org/plugins/pluginmc/gui/DropGui.java
+++ src/main/java/org/plugins/pluginmc/gui/DropGui.java
@@ -4,23 +4,15 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.InventoryAction;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 import org.plugins.pluginmc.Item;
 import org.plugins.pluginmc.Main;
 import org.plugins.pluginmc.api.DropApi;
-import org.plugins.pluginmc.events.BlockBreak;
 import org.plugins.pluginmc.objects.DropChance;
-import org.plugins.pluginmc.utils.ChatUtil;
 import org.plugins.pluginmc.utils.ItemBuilderUtil;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import static org.plugins.pluginmc.events.BlockBreak.drops;
 
