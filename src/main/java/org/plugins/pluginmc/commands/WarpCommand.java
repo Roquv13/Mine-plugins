e2e20bfc8493a075a33c4ae3fc37dc1be9571971
Roquv13 <czarekolszewski99@gmail.com>
2024-01-19 02:19:52 +0100

    Get inventory method

--- src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
@@ -1,4 +1,14 @@
 package org.plugins.pluginmc.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+
 public class WarpCommand {
+
+    public static Inventory getInventory(Player player) {
+        Inventory inventory = Bukkit.createInventory(player, 9, "Warps");
+
+        return inventory;
+    }
 }
