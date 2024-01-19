fa5cc96f2671299c92917392d16cbf8ddcb0608d
Roquv13 <czarekolszewski99@gmail.com>
2024-01-19 02:32:42 +0100

    Set up on command

--- src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
@@ -1,5 +1,21 @@
 package org.plugins.pluginmc.commands;
 
-public class WarpCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.plugins.pluginmc.gui.WarpGui;
 
+public class WarpCommand implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+       if (sender instanceof ConsoleCommandSender) return true;
+
+        Player player = (Player) sender;
+
+        player.openInventory(WarpGui.getInventory(player));
+
+        return false;
+    }
 }
