c5e8460e4caa95eaf7cc207b80af47211fcf7d5b
Roquv13 <czarekolszewski99@gmail.com>
2024-01-19 02:29:19 +0100

    Set up warps as items in Gui

--- src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
@@ -1,14 +1,5 @@
 package org.plugins.pluginmc.commands;
 
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
-
 public class WarpCommand {
 
-    public static Inventory getInventory(Player player) {
-        Inventory inventory = Bukkit.createInventory(player, 9, "Warps");
-
-        return inventory;
-    }
 }
--- src/main/java/org/plugins/pluginmc/gui/WarpGui.java
+++ src/main/java/org/plugins/pluginmc/gui/WarpGui.java
@@ -1,4 +1,39 @@
 package org.plugins.pluginmc.gui;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.plugins.pluginmc.utils.ItemBuilderUtil;
+
 public class WarpGui {
+
+    public static Inventory getInventory(Player player) {
+        Inventory inventory = Bukkit.createInventory(player, 9, "Warps");
+
+        //4,5,6
+        inventory.setItem(
+                4,
+                new ItemBuilderUtil(Material.ENCHANTING_TABLE, 1)
+                        .setName(ChatColor.LIGHT_PURPLE + "Enchants")
+                        .toItemStack()
+        );
+
+        inventory.setItem(
+                5,
+                new ItemBuilderUtil(Material.DIAMOND_SWORD, 1)
+                        .setName(ChatColor.AQUA + "Mob Grinder")
+                        .toItemStack()
+        );
+
+        inventory.setItem(
+                6,
+                new ItemBuilderUtil(Material.OAK_WOOD, 1)
+                        .setName(ChatColor.DARK_GREEN + "Oak Wood")
+                        .toItemStack()
+        );
+
+        return inventory;
+    }
 }
