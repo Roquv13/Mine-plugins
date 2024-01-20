85b6c4c49565658fce1056fb5abd8c6f56cb737e
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-20 12:45:42 +0100

    Display list of warps

--- src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/WarpCommand.java
@@ -1,11 +1,17 @@
 package org.plugins.pluginmc.commands;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.Player;
 import org.plugins.pluginmc.gui.WarpGui;
+
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
 
 public class WarpCommand implements CommandExecutor {
     @Override
@@ -14,11 +20,16 @@ public class WarpCommand implements CommandExecutor {
 
        Player player = (Player) sender;
 
-       if (args.length == 0) {
+        LinkedHashMap<String, Location> warps = new LinkedHashMap<>();
+
+        if (args.length == 0) {
            player.openInventory(WarpGui.getInventory(player));
        } else if (args.length == 1) {
            if (args[0].equalsIgnoreCase("list")) {
-
+               player.sendMessage(ChatColor.DARK_GREEN + "List of warps:");
+               for (String name : warps.keySet()) {
+                   player.sendMessage(ChatColor.GREEN + name);
+               }
            }
        } else if (args.length == 2) {
            if (args[0].equalsIgnoreCase("add")) {
