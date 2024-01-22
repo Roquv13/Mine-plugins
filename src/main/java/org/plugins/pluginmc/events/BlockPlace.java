f8a4ec4c20dd9256a14c3313191bbb401236892a
Roquv13 <czarekolszewski99@gmail.com>
2024-01-22 18:59:56 +0100

    Define block and locations of blocks

--- src/main/java/org/plugins/pluginmc/events/BlockPlace.java
+++ src/main/java/org/plugins/pluginmc/events/BlockPlace.java
@@ -1,5 +1,7 @@
 package org.plugins.pluginmc.events;
 
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
@@ -8,6 +10,8 @@ public class BlockPlace implements Listener {
 
     @EventHandler
     public void onBlockPlace(BlockPlaceEvent event) {
-
+        Block block = event.getBlock();
+        Location location1 = block.getLocation();
+        Location location2 = location1.clone().add(0, 1, 0);
     }
 }
