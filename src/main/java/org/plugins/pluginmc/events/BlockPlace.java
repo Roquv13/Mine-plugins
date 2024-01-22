c6c0b27898f7115307c6744d80fb18e2d94b9e8d
Roquv13 <czarekolszewski99@gmail.com>
2024-01-22 19:05:15 +0100

    Statement with block endstone for first location

--- src/main/java/org/plugins/pluginmc/events/BlockPlace.java
+++ src/main/java/org/plugins/pluginmc/events/BlockPlace.java
@@ -1,10 +1,13 @@
 package org.plugins.pluginmc.events;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockPlaceEvent;
+import org.plugins.pluginmc.Main;
 
 public class BlockPlace implements Listener {
 
@@ -13,5 +16,14 @@ public class BlockPlace implements Listener {
         Block block = event.getBlock();
         Location location1 = block.getLocation();
         Location location2 = location1.clone().add(0, 1, 0);
+
+        if (block.getType() == Material.END_STONE && location2.getBlock().getType() == Material.AIR) {
+            Bukkit.getScheduler().runTaskLater(Main.getInstance(), new Runnable() {
+                @Override
+                public void run() {
+                    location2.getBlock().setType(Material.STONE);
+                }
+            }, 20L);
+        }
     }
 }
