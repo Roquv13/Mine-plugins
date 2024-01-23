fcdc4f0fafd586a9610bae67822cb4b08caf271b
Roquv13 <czarekolszewski99@gmail.com>
2024-01-23 20:42:57 +0100

    Add removeHomeCommand and handle it

---
+++ src/main/java/org/plugins/pluginmc/commands/RemoveHomeCommand.java
@@ -0,0 +1,30 @@
+package org.plugins.pluginmc.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.plugins.pluginmc.api.HomeApi;
+
+public class RemoveHomeCommand implements CommandExecutor {
+
+    HomeApi homeApi = new HomeApi();
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        if (sender instanceof ConsoleCommandSender) return true;
+
+        Player player = (Player) sender;
+
+        if (args.length != 1) {
+            player.sendMessage("/removehome [home name]");
+            return true;
+        }
+
+        homeApi.removeHome(player, args[0]);
+
+        return false;
+    }
+}
