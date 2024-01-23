cc7ed96645bcb185aa82bf9d77d9dd690ceb7820
Roquv13 <czarekolszewski99@gmail.com>
2024-01-23 20:36:19 +0100

    Handle teleport player on command Home

--- src/main/java/org/plugins/pluginmc/commands/HomeCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/HomeCommand.java
@@ -1,12 +1,54 @@
 package org.plugins.pluginmc.commands;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.plugins.pluginmc.api.HomeApi;
+
+import java.util.Map;
 
 public class HomeCommand implements CommandExecutor {
+
+    HomeApi homeApi = new HomeApi();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        if (sender instanceof ConsoleCommandSender) return true;
+
+        Player player = (Player) sender;
+
+        if (homeApi.getHomesCount(player) == 0) {
+            player.sendMessage("You have no homes added.");
+            return true;
+        }
+
+        Map<String, Location> homes = homeApi.getHomes(player);
+
+        if (homeApi.getHomesCount(player) == 1) {
+            if (!homeApi.teleportHome(player, homes.entrySet().iterator().next().getKey())) {
+                player.sendMessage("Teleport to home failed.");
+            }
+            player.sendMessage("Successfully teleported to home.");
+            return true;
+        } else {
+            if (args.length != 1) {
+                player.sendMessage("Enter name of home.");
+                return true;
+            }
+
+            if (homeApi.homeExists(player, args[0])) {
+                player.sendMessage("Home with that name doesn't exists.");
+                return true;
+            }
+
+            player.teleport(homes.get(args[0]));
+            player.sendMessage("Successfully teleported to home.");
+        }
+
         return false;
     }
 }
