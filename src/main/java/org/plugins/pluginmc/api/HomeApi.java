f474fe5362c00dd74d9465adf704585cc20440e8
Roquv13 <czarekolszewski99@gmail.com>
2024-01-23 19:17:18 +0100

    Home Api added

---
+++ src/main/java/org/plugins/pluginmc/api/HomeApi.java
@@ -0,0 +1,15 @@
+package org.plugins.pluginmc.api;
+
+import org.plugins.pluginmc.Main;
+
+import javax.xml.stream.Location;
+import java.util.Map;
+
+public class HomeApi {
+
+    Main main = Main.getInstance();
+
+    public Map<String, Location> getHomes() {
+        
+    }
+}
