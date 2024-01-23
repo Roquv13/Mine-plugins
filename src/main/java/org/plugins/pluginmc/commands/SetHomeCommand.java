8b21f0b4599946233598fccec2699036ff58486d
Roquv13 <czarekolszewski99@gmail.com>
2024-01-23 20:41:19 +0100

    Handle setHome command

--- src/main/java/org/plugins/pluginmc/commands/SetHomeCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/SetHomeCommand.java
@@ -3,11 +3,28 @@ package org.plugins.pluginmc.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
+import org.plugins.pluginmc.api.HomeApi;
 
 public class SetHomeCommand implements CommandExecutor {
 
+    HomeApi homeApi = new HomeApi();
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+
+        if (sender instanceof ConsoleCommandSender) return true;
+
+        Player player = (Player) sender;
+
+        if (args.length != 1) {
+            player.sendMessage("/sethome [home name]");
+            return true;
+        }
+
+        homeApi.addHome(player, args[0], player.getLocation());
+
         return false;
     }
 }
