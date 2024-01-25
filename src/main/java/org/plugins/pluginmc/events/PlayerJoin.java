bb9847ca1a053fb55237627534f28250d292654a
Roquv13 <czarekolszewski99@gmail.com>
2024-01-25 04:28:32 +0100

    Add header and footer for tab list

--- src/main/java/org/plugins/pluginmc/events/PlayerJoin.java
+++ src/main/java/org/plugins/pluginmc/events/PlayerJoin.java
@@ -1,11 +1,17 @@
 package org.plugins.pluginmc.events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.plugins.pluginmc.Main;
 import org.plugins.pluginmc.utils.ChatUtil;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class PlayerJoin implements Listener {
 
@@ -18,5 +24,8 @@ public class PlayerJoin implements Listener {
         }
 
         player.sendMessage(ChatUtil.colorize(Main.configManager.getJoinMessage().replace("{PLAYER}", player.getName())));
+
+        List<String> header = new ArrayList<>(Arrays.asList(ChatColor.AQUA + "Discord: "));
+        List<String> footer = new ArrayList<>(Arrays.asList(ChatColor.GREEN + "Server IP: " + Bukkit.getIp()));
     }
 }
