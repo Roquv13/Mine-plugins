d47288871df44391447590d142d4f8d1078dafda
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-26 21:57:31 +0100

    Information for console

--- src/main/java/org/plugins/pluginmc/commands/GameModeCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/GameModeCommand.java
@@ -3,11 +3,17 @@ package org.plugins.pluginmc.commands;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.ConsoleCommandSender;
 
 public class GameModeCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        if (sender instanceof ConsoleCommandSender) {
+            System.out.println("Only for players.");
+            return true;
+        }
+
         return false;
     }
 }
