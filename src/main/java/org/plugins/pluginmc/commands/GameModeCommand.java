6107fd849abed9c15d8f71add1d3b2d31cea416a
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-26 21:59:35 +0100

    Message to player

--- src/main/java/org/plugins/pluginmc/commands/GameModeCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/GameModeCommand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.entity.Player;
 
 public class GameModeCommand implements CommandExecutor {
 
@@ -11,6 +12,13 @@ public class GameModeCommand implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         if (sender instanceof ConsoleCommandSender) {
             System.out.println("Only for players.");
+            return true;
+        }
+
+        Player player = (Player) sender;
+
+        if (!player.isOp()) {
+            player.sendMessage("You have no permission!");
             return true;
         }
 
