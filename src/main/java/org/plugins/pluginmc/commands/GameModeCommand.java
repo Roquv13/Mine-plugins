59ee81cc49bad686042b83db12849b38a22f0b05
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-26 21:55:12 +0100

    Implement method

--- src/main/java/org/plugins/pluginmc/commands/GameModeCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/GameModeCommand.java
@@ -1,6 +1,13 @@
 package org.plugins.pluginmc.commands;
 
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public class GameModeCommand implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
+        return false;
+    }
 }
