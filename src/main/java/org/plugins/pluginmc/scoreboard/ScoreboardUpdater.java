adeec8a5cf43f421baa343979ff5742b522f9dc9
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-27 17:57:57 +0100

    Method for create scoreboard

--- src/main/java/org/plugins/pluginmc/scoreboard/ScoreboardUpdater.java
+++ src/main/java/org/plugins/pluginmc/scoreboard/ScoreboardUpdater.java
@@ -1,4 +1,12 @@
 package org.plugins.pluginmc.scoreboard;
 
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.ScoreboardManager;
+
 public class ScoreboardUpdater {
+
+    public static void createScoreboard(Player player) {
+        ScoreboardManager manager = (ScoreboardManager) Bukkit.getScoreboardManager();
+    }
 }
