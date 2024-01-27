71f66f7d46962bf398ebd802ac5f3704e5df6c03
Cezary Olszewski <czarekolszewski99@gmail.com>
2024-01-27 18:18:14 +0100

    Creating scoreboard for player

--- src/main/java/org/plugins/pluginmc/scoreboard/ScoreboardUpdater.java
+++ src/main/java/org/plugins/pluginmc/scoreboard/ScoreboardUpdater.java
@@ -2,11 +2,16 @@ package org.plugins.pluginmc.scoreboard;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.bukkit.scoreboard.ScoreboardManager;
+import org.bukkit.scoreboard.*;
 
 public class ScoreboardUpdater {
 
     public static void createScoreboard(Player player) {
         ScoreboardManager manager = (ScoreboardManager) Bukkit.getScoreboardManager();
+        Scoreboard scoreboard = manager.getNewScoreboard();
+        Objective objective = scoreboard.registerNewObjective("Stats", Criteria.DUMMY, "Scoreboard");
+
+        objective.setDisplaySlot(DisplaySlot.SIDEBAR);
+        player.setScoreboard(scoreboard);
     }
 }
