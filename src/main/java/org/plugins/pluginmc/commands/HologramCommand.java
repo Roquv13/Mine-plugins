bbf43f1109fc11ec453436c39527db0ef8805b4b
Roquv13 <czarekolszewski99@gmail.com>
2024-01-29 19:36:09 +0100

    Create armor stand with visible set to false

--- src/main/java/org/plugins/pluginmc/commands/HologramCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/HologramCommand.java
@@ -4,16 +4,19 @@ import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 
 public class HologramCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
-        return false;
+        return true;
     }
 
     public void createHologram(Location location, String text) {
-        
+        ArmorStand armorStand = (ArmorStand) location.getWorld().spawnEntity(location, EntityType.ARMOR_STAND);
+        armorStand.setVisible(false);
     }
 }
