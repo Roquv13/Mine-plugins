67d8d8ea49d9b2e7a290f4a1c23716d9f10c9cee
Roquv13 <czarekolszewski99@gmail.com>
2024-01-29 19:17:23 +0100

    Add new text to motd

--- src/main/java/org/plugins/pluginmc/events/ServerListPing.java
+++ src/main/java/org/plugins/pluginmc/events/ServerListPing.java
@@ -1,5 +1,6 @@
 package org.plugins.pluginmc.events;
 
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
@@ -8,7 +9,10 @@ public class ServerListPing implements Listener {
 
     @EventHandler
     public void onServerListPing(ServerListPingEvent event) {
-        
+        event.setMotd(ChatColor.translateAlternateColorCodes('&',
+        "&7New play mode &dSky&fBlock" + "\n" +
+                "&7Server Discord:"
+        ));
     }
 
 }
