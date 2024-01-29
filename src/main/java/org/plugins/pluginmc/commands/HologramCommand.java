f0527e3b98cc5afacb2cc0e97ff60143935591a5
Roquv13 <czarekolszewski99@gmail.com>
2024-01-29 19:33:02 +0100

    Create hologram method

--- src/main/java/org/plugins/pluginmc/commands/HologramCommand.java
+++ src/main/java/org/plugins/pluginmc/commands/HologramCommand.java
@@ -1,13 +1,19 @@
 package org.plugins.pluginmc.commands;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class HologramCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String s, String[] args) {
         return false;
+    }
+
+    public void createHologram(Location location, String text) {
+        
     }
 }
