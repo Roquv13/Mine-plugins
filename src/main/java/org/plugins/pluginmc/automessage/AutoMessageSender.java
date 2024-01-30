b100254986a1a6c2b1e22887bdbb5bfab2dc4272
Roquv13 <czarekolszewski99@gmail.com>
2024-01-30 12:33:35 +0100

    Add messages to sender

--- src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
+++ src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
@@ -1,8 +1,18 @@
 package org.plugins.pluginmc.automessage;
 
+import org.bukkit.ChatColor;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class AutoMessageSender extends BukkitRunnable {
+
+    List<String> messages = Arrays.asList(
+            ChatColor.GRAY + "Discord server: " + ChatColor.AQUA + "XXXX",
+            ChatColor.GRAY + "Server site: " + ChatColor.AQUA + "XXXX",
+            ChatColor.GRAY + "Auto message: " + ChatColor.AQUA + "message"
+    );
 
     @Override
     public void run() {
