e807a79ad40d49613e132cd8be6280f8339f361b
Roquv13 <czarekolszewski99@gmail.com>
2024-01-30 13:08:00 +0100

    Reading from yaml file

--- src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
+++ src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
@@ -2,30 +2,43 @@ package org.plugins.pluginmc.automessage;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
-import java.util.Arrays;
+import java.io.File;
 import java.util.List;
 
 public class AutoMessageSender extends BukkitRunnable {
 
-    final List<String> messages = Arrays.asList(
-            ChatColor.GRAY + "Discord server: " + ChatColor.AQUA + "XXXX",
-            ChatColor.GRAY + "Server site: " + ChatColor.AQUA + "XXXX",
-            ChatColor.GRAY + "Auto message: " + ChatColor.AQUA + "message"
-    );
+    private final FileConfiguration config;
+    private final List<String> messages;
+    private final String prefix;
 
-    final String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "Info" + ChatColor.DARK_GRAY + "] ";
+    private int index = 0;
 
-    int i = 0;
+    public AutoMessageSender(JavaPlugin plugin, String filePath) {
+        this.config = loadConfig(plugin, filePath);
+        this.messages = config.getStringList("messages");
+        this.prefix = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "Info" + ChatColor.DARK_GRAY + "] ";
+    }
+
+    private FileConfiguration loadConfig(JavaPlugin plugin, String filePath) {
+        File file = new File(plugin.getDataFolder(), filePath);
+        if (!file.exists()) {
+            plugin.saveResource(filePath, false);
+        }
+        return YamlConfiguration.loadConfiguration(file);
+    }
 
     @Override
     public void run() {
-        i = (i + 1 < messages.size() ? i + 1 : 0);
+        index = (index + 1 < messages.size() ? index + 1 : 0);
 
         for (Player players : Bukkit.getOnlinePlayers()) {
-            players.sendMessage(prefix + messages.get(i));
+            players.sendMessage(prefix + messages.get(index));
         }
     }
 }
