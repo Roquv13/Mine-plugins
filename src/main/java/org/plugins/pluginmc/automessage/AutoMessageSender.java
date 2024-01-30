082c144cac262d261fd400ae3d86cd5245c81e2a
Roquv13 <czarekolszewski99@gmail.com>
2024-01-30 12:29:46 +0100

    Auto message to players

---
+++ src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
@@ -0,0 +1,11 @@
+package org.plugins.pluginmc.automessage;
+
+import org.bukkit.scheduler.BukkitRunnable;
+
+public class AutoMessageSender extends BukkitRunnable {
+
+    @Override
+    public void run() {
+
+    }
+}
