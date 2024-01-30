4b5892da59200b04dcef6c1183c91fe4a645177b
Roquv13 <czarekolszewski99@gmail.com>
2024-01-30 12:35:17 +0100

    Prefix for messages

--- src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
+++ src/main/java/org/plugins/pluginmc/automessage/AutoMessageSender.java
@@ -8,11 +8,13 @@ import java.util.List;
 
 public class AutoMessageSender extends BukkitRunnable {
 
-    List<String> messages = Arrays.asList(
+    final List<String> messages = Arrays.asList(
             ChatColor.GRAY + "Discord server: " + ChatColor.AQUA + "XXXX",
             ChatColor.GRAY + "Server site: " + ChatColor.AQUA + "XXXX",
             ChatColor.GRAY + "Auto message: " + ChatColor.AQUA + "message"
     );
+
+    final String prefix = ChatColor.DARK_GRAY + "[" + ChatColor.AQUA + "Info" + ChatColor.DARK_GRAY + "] ";
 
     @Override
     public void run() {
