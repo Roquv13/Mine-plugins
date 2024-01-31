29178e3b088d93419e5fc2d9c7d3659922e20363
Roquv13 <czarekolszewski99@gmail.com>
2024-01-31 16:37:49 +0100

    Farmer constructor

--- src/main/java/org/plugins/pluginmc/objects/Farmer.java
+++ src/main/java/org/plugins/pluginmc/objects/Farmer.java
@@ -1,4 +1,16 @@
 package org.plugins.pluginmc.objects;
 
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+
 public class Farmer {
+
+    private ItemStack farmerItem;
+    private Material holeGenerateType;
+
+    public Farmer(ItemStack farmerItem, Material holeGenerateType) {
+        this.farmerItem = farmerItem;
+        this.holeGenerateType = holeGenerateType;
+    }
+
 }
