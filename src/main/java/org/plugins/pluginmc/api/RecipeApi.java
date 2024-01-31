8bf592b76b63460d5edb6dd68c4e68b4d19547cd
Roquv13 <czarekolszewski99@gmail.com>
2024-01-31 16:41:18 +0100

    Get farmer method

--- src/main/java/org/plugins/pluginmc/api/RecipeApi.java
+++ src/main/java/org/plugins/pluginmc/api/RecipeApi.java
@@ -1,4 +1,15 @@
 package org.plugins.pluginmc.api;
 
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.inventory.ItemStack;
+import org.plugins.pluginmc.utils.ItemBuilderUtil;
+
 public class RecipeApi {
+
+    public ItemStack getFarmer(Material mainMaterial, String name) {
+        return new ItemBuilderUtil(mainMaterial, 1)
+                .setName(ChatColor.LIGHT_PURPLE + name)
+                .toItemStack();
+    }
 }
